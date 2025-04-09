7a139bdc54711a9123ec938bb1539eb069b3a757
Jamison Stalter <stalterjamison@gmail.com>
2025-04-09 11:21:31 -0700

    Initial commit: Flask project scaffold with secure config, login system, and database setup

--- app/app.py
+++ app/app.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from config import Config
 
-app = Flask(__name__)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'login'  # redirect if not logged in
+login_manager.login_message_category = 'info'
 
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
 
+    from app.routes import main
+    app.register_blueprint(main)
 
-if __name__ == '__main__':
-    app.run()
+    return app
---
+++ app/routes.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, render_template, redirect, url_for, flash
+from app.forms import LoginForm
+from app.models import User
+from app import db, bcrypt
+from flask_login import login_user, logout_user, login_required
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+    return render_template('index.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash('Logged in successfully!', 'Success')
+            return redirect(url_for('main.home'))
+        else:
+            flash('Login unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
