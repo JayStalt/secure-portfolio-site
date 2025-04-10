4764ee86a1180130fc710d6272b9ebc0e73dcd8e
Jamison Stalter <stalterjamison@gmail.com>
2025-04-10 10:12:19 -0700

    Add user registration with validation, password hashing, and duplicate account checks

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from app.models import User  # Make sure this import is at the top of forms.py
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 
@@ -6,3 +8,21 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That Username is already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already registered.')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from app.forms import LoginForm
+from app.forms import RegisterForm
 from app.models import User
 from app import db, bcrypt
 from flask_login import login_user, logout_user, login_required
+from flask_login import current_user
 
 main = Blueprint('main', __name__)
 
@@ -28,3 +30,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('main.home'))
+
+
+@main.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You can now log in.', 'success')
+        return redirect(url_for('main.login'))
+
+    return render_template('register.html', form=form)
