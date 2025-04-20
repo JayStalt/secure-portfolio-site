8e8b590dd2128514142ad0216037cb70d32a7a95
Jamison Stalter <stalterjamison@gmail.com>
2025-04-20 15:11:14 -0700

    Fix: Add db.create_all() to ensure tables are created on startup

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from config import Config
 import traceback
-from flask import jsonify
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = 'main.login'  # redirect if not logged in
+login_manager.login_view = 'main.login'
 login_manager.login_message_category = 'info'
 
 def create_app():
@@ -17,17 +16,19 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    bcrypt.init_app(app)
     login_manager.init_app(app)
 
     from app.routes import main
     app.register_blueprint(main)
 
-    import traceback
-    from flask import jsonify
-
     @app.errorhandler(500)
     def internal_error(error):
         app.logger.error(traceback.format_exc())
         return jsonify({"error": "Internal Server Error"}), 500
+
+    # Create tables if they don't exist
+    with app.app_context():
+        db.create_all()
 
     return app
