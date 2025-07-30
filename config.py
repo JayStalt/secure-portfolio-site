1e22d63e17c92a7acca6dc6ca6a44b196a6830b6
Jamison Stalter <stalterjamison@gmail.com>
2025-07-30 12:52:50 -0500

    Updated database path to persistent disk

--- config.py
+++ config.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# This is the path Render will mount the persistent disk to
+persistent_dir = '/var/data'
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'sqlite:///{os.path.join(persistent_dir, "site.db")}')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
