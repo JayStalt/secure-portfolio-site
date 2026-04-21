0e426c0127b58e7b0050eb7bd62c7fa83ab51602
Jamison Stalter <stalterjamison@gmail.com>
2026-04-20 19:25:38 -0500

    Fix local SQLite path to site.db

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////var/data/site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
